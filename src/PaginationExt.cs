b87099da0d4f39e74f375e31d039ce40ae0152c3
DarkCompet <darkcompet@gmail.com>
2022-07-04 09:49:12 +0700

    Add pagination ext

---
+++ src/PaginationExt.cs
@@ -0,0 +1,49 @@
+namespace Tool.Compet.Core;
+
+// Extension of IQueryable<T>
+// Where T: item type
+public static class PaginationExt {
+	/// @param pagePos From 1 (NOT from 0).
+	/// @param pageSize How many items in each page (for eg,. 10, 20, 50,...).
+	/// Note: given `pageIndex * pageSize` must be in range of Int32.
+	public static PagedResult<T> PaginateDk<T>(this IQueryable<T> queryable, int pagePos = 1, int pageSize = 50) where T : class {
+		// Maybe zero-
+		if (pagePos < 1) {
+			pagePos = 1;
+		}
+
+		// Maybe overflow
+		var offset = pagePos * pageSize;
+		if (offset < 0) {
+			offset = 0;
+		}
+
+		var items = queryable.Skip(offset).Take(pageSize).ToArray() ?? new T[0];
+
+		// This calculation is faster than `Math.Ceiling(items.Length / pageSize)`
+		var pageCount = (items.Length + pageSize - 1) / pageSize;
+
+		return new PagedResult<T>(
+			items: items,
+			pagePos: pagePos,
+			pageCount: pageCount
+		);
+	}
+}
+
+public class PagedResult<T> where T : class {
+	/// Item list in the page
+	public readonly T[] items;
+
+	/// Position (1-index-based) of current page
+	public readonly int pagePos;
+
+	/// Total number of page
+	public readonly int pageCount;
+
+	public PagedResult(T[] items, int pagePos, int pageCount) {
+		this.items = items;
+		this.pagePos = pagePos;
+		this.pageCount = pageCount;
+	}
+}
